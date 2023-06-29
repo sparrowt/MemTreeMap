6c380190bd823500642427f9d4cbff7995b11bd8
Tom Sparrow <793763+sparrowt@users.noreply.github.com>
2023-06-29 13:24:56 +0100

    Bugfixes, comments, hints
    - Fix to handle `,` as thousands separator not just `.`
    - Use latest Google Charts source
    - Code comments
    - In-page hints how to use the graph

--- MemTreeMap.pl
+++ MemTreeMap.pl
@@ -1,63 +1,101 @@
-# Original from https://juanpalomez.wordpress.com/2013/04/18/memory-usage-treemap-windirstat-for-memory/
+# Memory usage visualisation (like WinDirStat for RAM)
+#
+# Heavily inspired by https://juanpalomez.wordpress.com/2013/04/18/memory-usage-treemap-windirstat-for-memory/
+# Bug fixes, enhancements, comments by sparrowt (who barely knows perl but hasn't yet bothered to rewrite it in python)
+#
+# Usage:
+#   perl MemTreeMap.pl > memoryusage.html && start memoryusage.html
+#
+# From powershell:
+#   perl MemTreeMap.pl | Out-File -Encoding Utf8 memoryusage.html ; start memoryusage.html
+#
+# Notes:
+#  - this does NOT show some memory usage such as 'Driver Locked' (e.g. by Hyper-V) use SysInternals RamMap to show this
+#  - to show PID breakdown without clicking change `maxPostDepth: 1` to `maxDepth: 2`
+
+use POSIX 'strftime';
 
 my %groups = ();
-my $output;
+my $output_data;
 
-my $output_pre = "
+my $output_A = q`
         <html>
           <head>
-            <script type='text/javascript' src='https://www.google.com/jsapi'></script>
+            <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
             <script type='text/javascript'>
-              google.load('visualization', '1', {packages:['treemap']});
-              google.setOnLoadCallback(drawChart);
+              google.charts.load('current', {'packages':['treemap']});
+              google.charts.setOnLoadCallback(drawChart);
               function drawChart() {
                   // Create and populate the data table.
                   var data = google.visualization.arrayToDataTable([
                     ['Process', 'Parent', 'Size'],
-                    ['root',    null,                 0],
-";
+                    ['All Processes',    null,                 0],
+`;
+# The `while` loop below fills in the rest of the data array
 
-my $output_post = "
+my $output_B = q`
                   ]);
 
                   // Create and draw the visualization.
                   var treemap = new google.visualization.TreeMap(document.getElementById('chart_div'));
                   treemap.draw(data, {
-                    minColor: 'red',
-                    midColor: '#ddd',
-                    maxColor: '#0d0',
+                    highlightOnMouseOver: true,
+                    maxPostDepth: 1,
+                    minColor: '#baa',
+                    midColor: '#b60',
+                    maxColor: '#e00',
+                    showScale: true,
+                    hintOpacity: 0.0,
                     headerHeight: 15,
                     fontColor: 'black',
-                    showScale: true});
+                  });
                 }
         </script>
   </head>
+`;
 
+my $dt_str = strftime("%Y-%m-%d %H:%M:%S UTC", gmtime);
+
+my $output_C = "
   <body>
-    <div id='chart_div' style='width: 900px; height: 500px;'></div>
+    <h2>Memory Usage Visualization at $dt_str</h2>
+    <div id='chart_div' style='width: 1200px; height: 900px;'></div>
+    <p>Tips:
+        <ul>
+            <li>At the top level memory usage is aggregated by executable name</li>
+            <li>Left-click to drill down to individual process IDs</li>
+            <li>Right-click to return up a level</li>
+        </ul>
+    </p>
   </body>
 </html>
 ";
 
 open TASKLIST, "tasklist /nh /fo CSV |" or die "Can't execute tasklist command $!\n";
 while (<TASKLIST>) {
+        # tasklist output columns: "Image Name","PID","Session Name","Session#","Mem Usage"
         if (m/"(.+)","(.+)",".+",".+","(.+) KB?"/) {
                 $name = $1;
                 $pid  = $2;
                 $size = $3;
-                $size =~ s/\.//;
-                $output .= "['$pid','$name',$size],\n";
+                # Remove all thousands separators
+                $size =~ s/[\.,]//g;
+                # Add per-process node as a child of the executable name
+                $output_data .= "['$pid','$name',$size],\n";
+                # Ensure exe name is in the set of groups (value 1 is ignored)
                 $groups{"$name"} = 1;
         }
 }
 
 close TASKLIST;
 
+# Add each group (processes by exe name) as children of the root 'All Processes' node
+# Set size 0 but they are sized based on the sum of all their children
 while ( my ($key, $value) = each(%groups) ) {
-        $output .= "['$key','root',0],\n";
+        $output_data .= "['$key','All Processes',0],\n";
 }
 
-chop $output;
-chop $output;
+chop $output_data;
+chop $output_data;
 
-print $output_pre . $output . $output_post;
+print $output_A . $output_data . $output_B . $output_C;
